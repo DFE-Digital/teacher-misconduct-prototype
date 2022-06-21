67b4fefa1e8664738035ce56b0531f0f26f66f11
Paul Hayes <fofr@users.noreply.github.com>
2022-06-21 14:10:15 +0100

    Use warn on unsaved changes from govuk-prototype-components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,20 +2,16 @@
 import '../stylesheets/application.scss'
 
 // Import GOV.UK Frontend
-import { initAll as govukFrontend } from 'govuk-frontend'
+import { initAll as GOVUKFrontend } from 'govuk-frontend'
 
 // Import GOV.UK Prototype Rig
-import GOVUKPrototypeComponents from 'govuk-prototype-components'
+import { initAll as GOVUKPrototypeComponents } from 'govuk-prototype-components'
 
 import { Calendar } from './calendar'
-import WarnOnUnsavedChanges from './warn-on-unsaved-changes'
 
 // Initiate scripts on page load
 document.addEventListener('DOMContentLoaded', () => {
-  govukFrontend()
-
-  GOVUKPrototypeComponents.WarnOnUnsavedChanges = WarnOnUnsavedChanges
-  GOVUKPrototypeComponents.initAll()
-
+  GOVUKFrontend()
+  GOVUKPrototypeComponents()
   Calendar()
 })
