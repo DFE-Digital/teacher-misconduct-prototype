6d1a34e7aaa8b5d0c838524d82a03ab87cb9f71a
Paul Hayes <fofr@users.noreply.github.com>
2022-06-27 18:04:20 +0100

    Add a teacher details section

--- app/wizards/report/teacher-details-wizard.js
+++ app/wizards/report/teacher-details-wizard.js
@@ -4,9 +4,9 @@ export function teacherDetailsWizard (req) {
   const journey = {
     '/report/tasks': {},
     '/report/teacher/name': {},
-    // '/report/teacher/age': {},
-    // '/report/teacher/nino': {},
-    // '/report/teacher/trn': {},
+    '/report/teacher/age': {},
+    '/report/teacher/nino': {},
+    '/report/teacher/trn': {},
     '/report/teacher/check-answers': {
       '/report/tasks': true
     }
