84ebb7c7ab7f8955ea2752648991fb5115941f04
Paul Hayes <fofr@users.noreply.github.com>
2022-06-27 17:27:43 +0100

    Add initial task list

--- app/routes/report.js
+++ app/routes/report.js
@@ -1,15 +1,27 @@
-import { reportWizard } from '../wizards/report-wizard.js'
+// import { reportWizard } from '../wizards/report-wizard.js'
+import { teacherDetailsWizard } from '../wizards/report/teacher-details-wizard.js'
 
 export const reportRoutes = router => {
+  // router.all([
+  //   '/report',
+  //   '/report/:view'
+  // ], (req, res, next) => {
+  //   res.locals.paths = reportWizard(req)
+  //   next()
+  // })
+
   router.all([
-    '/report',
-    '/report/:view'
+    '/report/teacher',
+    '/report/teacher/:view'
   ], (req, res, next) => {
-    res.locals.paths = reportWizard(req)
+    res.locals.paths = teacherDetailsWizard(req)
     next()
   })
 
-  router.post('/report/:view', (req, res) => {
+  router.post([
+    '/report/:view',
+    '/report/teacher/:view'
+  ], (req, res) => {
     res.redirect(res.locals.paths.next)
   })
 }
---
+++ app/wizards/report/teacher-details-wizard.js
@@ -0,0 +1,16 @@
+import { wizard } from 'govuk-prototype-rig'
+
+export function teacherDetailsWizard (req) {
+  const journey = {
+    '/report/tasks': {},
+    '/report/teacher/name': {},
+    // '/report/teacher/age': {},
+    // '/report/teacher/nino': {},
+    // '/report/teacher/trn': {},
+    '/report/teacher/check-answers': {
+      '/report/tasks': true
+    }
+  }
+
+  return wizard(journey, req)
+}
